686cf13b975ec6ff92eb97a98d889cb23a1097e5
smthwsmth <smthwsmth@gmail.com>
2022-11-29 22:09:38 +0500

    Some new responses were implemented

--- telegrambot.py
+++ telegrambot.py
@@ -7,6 +7,10 @@ def start(message):
     mess = f'<b>Привет, мигрант {message.from_user.first_name}!</b> Добро пожаловать в страну!'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
+@bot.message_handler()
+def get_user_text(message):
+    bot.send_message(message.chat.id, message, parse_mode='html')
+    print(message.chat.from_user[id])
 
 
 
